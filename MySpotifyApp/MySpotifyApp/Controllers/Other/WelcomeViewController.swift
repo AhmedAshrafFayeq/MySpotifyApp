6ab255f50dbd020b4cded86024ccd407b24c040b
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2022-02-14 02:00:27 +0200

    connect welcomeVC with AuthVC, add some extensions in UIView

--- MySpotifyApp/MySpotifyApp/Controllers/Other/WelcomeViewController.swift
+++ MySpotifyApp/MySpotifyApp/Controllers/Other/WelcomeViewController.swift
@@ -8,10 +8,34 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-
+    
+    let signInButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .white
+        button.setTitle("Sign in with Spotify", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Spotify"
         view.backgroundColor = .systemGreen
+        view.addSubview(signInButton)
+        signInButton.addTarget(self, action: #selector(didTapSignIn), for: .touchUpInside)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        signInButton.frame = CGRect(x: 20,
+                                    y: view.height-50-view.safeAreaInsets.bottom,
+                                    width: view.width-40,
+                                    height: 50)
+    }
+    
+    @objc func didTapSignIn() {
+        let AuthVC = AuthViewController()
+        AuthVC.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(AuthVC, animated: true)
     }
 }
--- MySpotifyApp/MySpotifyApp/Managers/AuthManager.swift
+++ MySpotifyApp/MySpotifyApp/Managers/AuthManager.swift
@@ -11,6 +11,11 @@ class AuthManager {
     
     static let shared = AuthManager()
     
+    struct Constants{
+        static let ClientID = "a8c0410ca32145348527d31c05eb4229"
+        static let ClientSecret = "0e29c6ed26d4468aaba5fe9c2cbce9fa"
+    }
+    
     private init(){}
     
     var isSignedIn: Bool{
