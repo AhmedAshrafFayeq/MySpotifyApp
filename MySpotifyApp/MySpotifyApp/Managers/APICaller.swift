e53747633edb385cb7993ece92be4c2d05065c78
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2022-03-05 21:59:42 +0200

    decode UserProfile api json to a UserProfile model

--- MySpotifyApp/MySpotifyApp/Managers/APICaller.swift
+++ MySpotifyApp/MySpotifyApp/Managers/APICaller.swift
@@ -32,10 +32,11 @@ final class APICaller {
                 }
                 
                 do{
-                    let result = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed)
+                    let result = try JSONDecoder().decode(UserProfile.self, from: data)
                     print(result)
                 }
                 catch{
+                    print(error.localizedDescription)
                     completion(.failure(error))
                 }
             }.resume()
