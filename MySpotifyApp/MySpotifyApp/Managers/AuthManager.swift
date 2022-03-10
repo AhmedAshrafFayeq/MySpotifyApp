5659aa6e349ef61888d9a2f673f2a4086bc2fdc9
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2022-03-10 23:16:56 +0200

    edit refrshtoken

--- MySpotifyApp/MySpotifyApp/Managers/AuthManager.swift
+++ MySpotifyApp/MySpotifyApp/Managers/AuthManager.swift
@@ -121,12 +121,12 @@ class AuthManager {
         }
     }
     
-    public func refreshIfNeeded(completion: @escaping (Bool) -> Void){
+    public func refreshIfNeeded(completion: ((Bool) -> Void)?){
         
         guard !refreshingToken else { return }
         
         guard shouldRefreshToken else {
-            completion(true)
+            completion?(true)
             return
         }
         
@@ -152,7 +152,7 @@ class AuthManager {
         let data = basicToken.data(using: .utf8)
         guard let base64String = data?.base64EncodedString() else {
             print("Failure to get base64")
-            completion(false)
+            completion?(false)
             return
         }
         request.setValue("Basic \(base64String)", forHTTPHeaderField: "Authorization")
@@ -161,7 +161,7 @@ class AuthManager {
             guard let self = self else {return}
             self.refreshingToken = false
             guard let data = data, error == nil else {
-                completion(false)
+                completion?(false)
                 return
             }
             do {
@@ -170,10 +170,10 @@ class AuthManager {
                 self.onRefreshBlocks.forEach { $0(result.access_token)}
                 self.onRefreshBlocks.removeAll()
                 self.cacheToken(result: result)
-                completion(true)
+                completion?(true)
             }catch{
                 print(error.localizedDescription)
-                completion(false)
+                completion?(false)
             }
         }.resume()
         
