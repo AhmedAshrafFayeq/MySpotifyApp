338a20b3c8cddd15a1e487e6bb0e629ac082fea6
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2022-03-16 11:03:36 +0200

    add home collection view and spinner activity indicator

--- MySpotifyApp/MySpotifyApp/Controllers/Core/HomeViewController.swift
+++ MySpotifyApp/MySpotifyApp/Controllers/Core/HomeViewController.swift
@@ -8,7 +8,20 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    private var collectionView: UICollectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: UICollectionViewCompositionalLayout{ sectionIndex, _ -> NSCollectionLayoutSection in
+            return HomeViewController.createSectionLayout(section: sectionIndex)
+        } ) 
+    
+    private let spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView()
+        spinner.backgroundColor = .label
+        spinner.hidesWhenStopped = true
+        return spinner
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Browse"
@@ -17,12 +30,51 @@ class HomeViewController: UIViewController {
                                                             style: .done,
                                                             target: self,
                                                             action: #selector(didTapSettings))
-        
+        view.addSubview(spinner)
+        configureCollectionView()
         fetchData()
         
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView.frame = view.bounds
+    }
+    
+    private func configureCollectionView(){
+        view.addSubview(collectionView)
+        collectionView.register(UICollectionViewCell.self,
+                                forCellWithReuseIdentifier: "cell")
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.backgroundColor = .systemBackground
+    }
+    
+    private static func createSectionLayout(section: Int) -> NSCollectionLayoutSection{
+        // Item
+        let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalHeight(1.0))
+        )
+        
+        // Group
+        let group = NSCollectionLayoutGroup.vertical(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .absolute(120)
+            ),
+            subitem: item,
+            count: 1)
+        
+        // Section
+        let section = NSCollectionLayoutSection(group: group)
+        return section
+    }
+    
     private func fetchData(){
+        // Featured Playlists
+        // Recommended Tracks
+        // New Releases
         APICaller.shared.getRecommendedGenres { result in
             switch result {
             case .success(let model):
@@ -33,20 +85,32 @@ class HomeViewController: UIViewController {
                         seeds.insert(random)
                     }
                 }
-                
                 APICaller.shared.getRecommendations(genres: seeds) { _ in
                     
                 }
-                
             case .failure(_): break
             }
         }
     }
-
+    
     @objc func didTapSettings(){
         let profileViewController = SettingsViewController()
         profileViewController.title = "Settings"
         profileViewController.navigationItem.largeTitleDisplayMode = .never
         navigationController?.pushViewController(profileViewController, animated: true)
+    }
+}
+
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .systemGreen
+        
+        return cell
     }
 }
