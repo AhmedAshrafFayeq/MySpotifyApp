12dce690e53d67f21a7e427c14877d66103884ea
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2022-03-19 14:19:54 +0200

    finalize new release cell & display it on HomeVC

--- MySpotifyApp/MySpotifyApp/Views/Browse/NewReleaseCollectionViewCell.swift
+++ MySpotifyApp/MySpotifyApp/Views/Browse/NewReleaseCollectionViewCell.swift
@@ -20,21 +20,21 @@ class NewReleaseCollectionViewCell: UICollectionViewCell {
     
     private let albumNameLabel: UILabel = {
         let label   = UILabel()
-        label.font  = .systemFont(ofSize: 22, weight: .semibold)
+        label.font  = .systemFont(ofSize: 20, weight: .semibold)
         label.numberOfLines = 0
         return label
     }()
     
     private let numberOfTracksLabel: UILabel = {
         let label   = UILabel()
-        label.font  = .systemFont(ofSize: 18, weight: .light)
+        label.font  = .systemFont(ofSize: 18, weight: .thin)
         label.numberOfLines = 0
         return label
     }()
     
     private let artistNameLabel: UILabel = {
         let label   = UILabel()
-        label.font  = .systemFont(ofSize: 18, weight: .regular)
+        label.font  = .systemFont(ofSize: 18, weight: .light)
         label.numberOfLines = 0
         return label
     }()
@@ -55,24 +55,40 @@ class NewReleaseCollectionViewCell: UICollectionViewCell {
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        albumNameLabel.sizeToFit()
-        numberOfTracksLabel.sizeToFit()
+        let imageSize: CGFloat = contentView.height-10
+        let albumNameLabelSize = albumNameLabel.sizeThatFits(
+            CGSize(width: contentView.width-imageSize-10,
+                   height: contentView.height-10
+                  )
+        )
+        artistNameLabel.sizeToFit()
         numberOfTracksLabel.sizeToFit()
         
-        let imageSize: CGFloat = contentView.height-10
+        //Image
         albumCoverImageView.frame = CGRect(x: 5, y: 5, width: imageSize, height: imageSize)
         
+        //Album name label
+        let albumLabelHeight = min(60, albumNameLabelSize.height)
         albumNameLabel.frame = CGRect(
             x: albumCoverImageView.right+10,
             y: 5,
-            width: albumNameLabel.width,
-            height: albumNameLabel.height)
+            width: albumNameLabelSize.width,
+            height: albumLabelHeight
+        )
+        
+        artistNameLabel.frame = CGRect(
+            x: albumCoverImageView.right+10,
+            y: albumNameLabel.bottom,
+            width: contentView.width - albumCoverImageView.right-10,
+            height: 30
+        )
         
         numberOfTracksLabel.frame = CGRect(
             x: albumCoverImageView.right+10,
-            y: albumCoverImageView.bottom-50,
+            y: contentView.bottom-44,
             width: numberOfTracksLabel.width,
-            height: 50)
+            height: 44
+        )
     }
     
     override func prepareForReuse() {
