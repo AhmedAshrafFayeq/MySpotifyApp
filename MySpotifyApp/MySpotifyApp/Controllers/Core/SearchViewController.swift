fcbea589c6e6103ce86a234eb6d0a4e53f3fb051
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2022-03-31 17:29:41 +0200

    add basic components for SearchVC

--- MySpotifyApp/MySpotifyApp/Controllers/Core/SearchViewController.swift
+++ MySpotifyApp/MySpotifyApp/Controllers/Core/SearchViewController.swift
@@ -10,27 +10,79 @@ import UIKit
 class SearchViewController: UIViewController, UISearchResultsUpdating{
     
     let searchController: UISearchController = {
-        let results = UIViewController()
-        results.view.backgroundColor = .red
-        let vc = UISearchController(searchResultsController: results)
+
+        let vc = UISearchController(searchResultsController: SearchResultsViewController())
         vc.searchBar.placeholder = "Songs, Artists, Albums"
         vc.searchBar.searchBarStyle = .minimal
         vc.definesPresentationContext = true
         return vc
     }()
-
+    
+    private let collectionView: UICollectionView = UICollectionView(
+        frame: .zero
+        , collectionViewLayout: UICollectionViewCompositionalLayout(sectionProvider: { _, _ -> NSCollectionLayoutSection? in
+            let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1)))
+            
+            item.contentInsets = NSDirectionalEdgeInsets(
+                top: 2,
+                leading: 2,
+                bottom: 2,
+                trailing: 2)
+            
+            let group = NSCollectionLayoutGroup.horizontal(
+                layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                                   heightDimension: .absolute(180)),
+                subitems: [item, item]
+            )
+            return NSCollectionLayoutSection(group: group)
+        }))
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         searchController.searchResultsUpdater = self
         navigationItem.searchController = searchController
+        view.addSubview(collectionView)
+        collectionView.register(UICollectionViewCell.self,
+                                forCellWithReuseIdentifier: "cell")
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.backgroundColor = .systemBackground
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView.frame = view.bounds
     }
     
     func updateSearchResults(for searchController: UISearchController) {
-        guard let query = searchController.searchBar.text,
+        guard let resultsController = searchController.searchResultsController as? SearchResultsViewController,
+              let query = searchController.searchBar.text,
               !query.trimmingCharacters(in: .whitespaces).isEmpty else {
                   return
               }
+        // resultsController.update(with: results)
+        print(query)
         //Perform search
+        //APICaller.shared.search
     }
+}
+
+extension SearchViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .gray
+        return cell
+    }
+    
 }
