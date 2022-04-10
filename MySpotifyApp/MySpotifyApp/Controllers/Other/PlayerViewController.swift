6b1fe9d9d5b6bbe784f75ed37344d850aecde282
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2022-04-10 20:24:29 +0200

    add PlayerControlsView

--- MySpotifyApp/MySpotifyApp/Controllers/Other/PlayerViewController.swift
+++ MySpotifyApp/MySpotifyApp/Controllers/Other/PlayerViewController.swift
@@ -15,17 +15,25 @@ class PlayerViewController: UIViewController {
         imageView.backgroundColor = .systemBlue
         return imageView
     }()
+    
+    private let controlsView = PlayerControlsView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        view.addSubviews(imageView)
+        view.addSubviews(imageView, controlsView)
         configureBarButtons()
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         imageView.frame = CGRect(x: 0, y: view.safeAreaInsets.top, width: view.width, height: view.width)
+        controlsView.frame = CGRect(
+            x: 10,
+            y: imageView.bottom+10,
+            width: view.width-20,
+            height: view.height-imageView.height-view.safeAreaInsets.top-view.safeAreaInsets.bottom-15
+        )
     }
     
     private func configureBarButtons() {
