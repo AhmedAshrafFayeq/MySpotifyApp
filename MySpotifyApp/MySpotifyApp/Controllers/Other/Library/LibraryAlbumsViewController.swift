304cb121bc5be641404f4ff10d63592682ba2fe8
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2022-04-21 21:00:41 +0200

    add LibraryPlayListVC and LibraryAlbumsVC

---
+++ MySpotifyApp/MySpotifyApp/Controllers/Other/Library/LibraryAlbumsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LibraryAlbumsViewController.swift
+//  MySpotifyApp
+//
+//  Created by Ahmed Fayeq on 21/04/2022.
+//
+
+import UIKit
+
+class LibraryAlbumsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
---
+++ MySpotifyApp/MySpotifyApp/Controllers/Other/Library/LibraryPlaylistsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryPlaylistsViewController.swift
+//  MySpotifyApp
+//
+//  Created by Ahmed Fayeq on 21/04/2022.
+//
+
+import UIKit
+
+class LibraryPlaylistsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+    
+}
