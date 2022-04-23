0848e9cd26b75748b3964905f6cb5cf69bede920
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2022-04-23 22:36:23 +0200

    add some enhancement in LibraryVC

--- MySpotifyApp/MySpotifyApp/Controllers/Core/LibraryViewController.swift
+++ MySpotifyApp/MySpotifyApp/Controllers/Core/LibraryViewController.swift
@@ -11,7 +11,7 @@ class LibraryViewController: UIViewController {
     
     private let playlistVC = LibraryPlaylistsViewController()
     private let albumsVC = LibraryAlbumsViewController()
-
+    
     private let scrollView: UIScrollView = {
         let scrollView = UIScrollView()
         scrollView.isPagingEnabled = true
@@ -21,11 +21,25 @@ class LibraryViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        scrollView.delegate = self
         view.addSubview(scrollView)
+        scrollView.backgroundColor = .systemYellow
     }
-
+    
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        scrollView.frame = CGRect(
+            x: 0,
+            y: view.safeAreaInsets.top+55,
+            width: view.width,
+            height: view.height-view.safeAreaInsets.top-view.safeAreaInsets.bottom-55
+        )
+        
+    }
+}
+
+extension LibraryViewController: UIScrollViewDelegate {
+    func scrollViewDidScrollToTop(_ scrollView: UIScrollView) {
         
     }
 }
