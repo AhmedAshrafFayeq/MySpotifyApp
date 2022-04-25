f0328438b731234712139fa92bf592e6c53897c6
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2022-04-25 16:59:43 +0200

    add view indicator in toggleview

--- MySpotifyApp/MySpotifyApp/Views/LibraryToggleView.swift
+++ MySpotifyApp/MySpotifyApp/Views/LibraryToggleView.swift
@@ -12,9 +12,16 @@ protocol LibraryToggleViewDelegate: AnyObject {
     func libraryToggleViewDidTapAlbums(_ toggleView: LibraryToggleView)
 }
 
+enum State {
+    case playlists
+    case albums
+}
+
 class LibraryToggleView: UIView {
     
     weak var delegate: LibraryToggleViewDelegate?
+    
+    var state: State = .playlists
     
     private let playListButton: UIButton = {
         let button = UIButton()
@@ -29,10 +36,18 @@ class LibraryToggleView: UIView {
         button.setTitle("Albums", for: .normal)
         return button
     }()
+    
+    private let indicatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemGreen
+        view.layer.masksToBounds = true
+        view.layer.cornerRadius = 4
+        return view
+    }()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        addSubviews(playListButton, albumsButton)
+        addSubviews(playListButton, albumsButton, indicatorView)
         
         playListButton.addTarget(self, action: #selector(didTapPlaylists), for: .touchUpInside)
         albumsButton.addTarget(self, action: #selector(didTapAlbums), for: .touchUpInside)
@@ -43,18 +58,36 @@ class LibraryToggleView: UIView {
     }
     
     @objc private func didTapPlaylists() {
+        state = .playlists
+        UIView.animate(withDuration: 0.2) {
+            self.layoutSubviews()
+        }
         delegate?.libraryToggleViewDidTapPlaylists(self)
     }
     
     @objc private func didTapAlbums() {
+        state = .albums
+        UIView.animate(withDuration: 0.2) {
+            self.layoutSubviews()
+        }
         delegate?.libraryToggleViewDidTapAlbums(self)
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        playListButton.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
-        albumsButton.frame = CGRect(x: playListButton.right, y: 0, width: 100, height: 50)
-        
+        playListButton.frame = CGRect(x: 0, y: 0, width: 100, height: 40)
+        albumsButton.frame = CGRect(x: playListButton.right, y: 0, width: 100, height: 40)
+        layoutIndicator()
+    }
+    
+    private func layoutIndicator() {
+        switch state {
+        case .playlists:
+            indicatorView.frame = CGRect(x: 0, y: playListButton.bottom, width: 100, height: 3)
 
+        case .albums:
+            indicatorView.frame = CGRect(x: 100, y: playListButton.bottom, width: 100, height: 3)
+
+        }
     }
 }
