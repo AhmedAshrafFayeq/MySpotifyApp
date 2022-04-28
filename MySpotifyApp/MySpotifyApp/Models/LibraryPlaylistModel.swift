cfce001d91a95bbc2a0e6effeced7380a8a0c793
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2022-04-28 23:26:03 +0200

    add LibraryPlaylistsModel

---
+++ MySpotifyApp/MySpotifyApp/Models/LibraryPlaylistModel.swift
@@ -0,0 +1,12 @@
+//
+//  LibraryPlaylistsModel.swift
+//  MySpotifyApp
+//
+//  Created by Ahmed Fayeq on 28/04/2022.
+//
+
+import Foundation
+
+struct LibraryPlaylistsModel: Codable {
+    let item: [Playlist]
+}
