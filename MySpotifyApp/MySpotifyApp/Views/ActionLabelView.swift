b01aec20f32a85f34ae4d3c4f5f4d5ebfb8fafa2
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2022-04-29 19:18:57 +0200

    add ActionLabelView

---
+++ MySpotifyApp/MySpotifyApp/Views/ActionLabelView.swift
@@ -0,0 +1,24 @@
+//
+//  ActionLabelView.swift
+//  MySpotifyApp
+//
+//  Created by Ahmed Fayeq on 29/04/2022.
+//
+
+import UIKit
+
+class ActionLabelView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        clipsToBounds = true
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+}
