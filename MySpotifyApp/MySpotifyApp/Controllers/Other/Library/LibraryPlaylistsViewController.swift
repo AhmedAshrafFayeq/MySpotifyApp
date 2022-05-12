82dbed6fe124ece3a599f26b15e1c630425f5ca2
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2022-05-12 14:06:25 +0200

    open playlist in new vc

--- MySpotifyApp/MySpotifyApp/Controllers/Other/Library/LibraryPlaylistsViewController.swift
+++ MySpotifyApp/MySpotifyApp/Controllers/Other/Library/LibraryPlaylistsViewController.swift
@@ -141,6 +141,13 @@ extension LibraryPlaylistsViewController: UITableViewDelegate, UITableViewDataSo
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let vc = PlayListViewController(playlist: playlists[indexPath.row])
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         70
     }
